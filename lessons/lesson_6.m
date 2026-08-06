35aafc746b38c594ac651139e08d8324d41c270e
Jonathan Gould <jonathan.d.gould@gmail.com>
2026-08-06 11:47:41 -0400

    minor reorganization

--- lessons/lesson_6.m
+++ lessons/lesson_6.m
@@ -1,4 +1,8 @@
-%% ----------------------- PROBLEM SET #4 -----------------------
+clc; clear;
+
+% data is here: https://github.com/Jonathan-Gould/ngp-matlab-bootcamp
+
+%% ----------------------- Lesson 6 -----------------------
 
 % This dataset contains the following 5 variables:
 
@@ -15,17 +19,12 @@
 % -Lick_full: a trace showing when the mouse's tongue broke an IR beam
 % (0=no lick, 1=lick)
 
-%% ----------------------- PROBLEM 4.1 -----------------------
-% Plot each variable to inspect the data
-
-
+%% Plot each variable to inspect the data
 
 plot(Fluorescence)
 
 
-
-%% ----------------------- PROBLEM 4.2 -----------------------
-% Process the Fluorescence data
+%% Process the Fluorescence data
 
 % Delta F over F (dF/F) - each point in the fluorescence trace should have
 % the F0 subtracted, then divided. In this case let's set the F0 as the 
@@ -36,24 +35,20 @@ normalized_flouresence = (Fluorescence - F0) / F0;
 
 
 
-%% ----------------------- PROBLEM 4.3 -----------------------
-% Plot the change in fluorescence around ensure availability
+%% Plot the change in fluorescence around ensure availability
 % how long before/after each ensure reward do you want to look?
 
 
 
-
-
-%% ----------------------- PROBLEM 4.4 ----------------------- 
-% Demonstrate how the fluorescence changes in response to Ensure rewards
+%% Plot the change in fluorescence around licks
 
 
 
+%% Quantify how the fluorescence changes in response to Ensure rewards
 
 
 
-%% ----------------------- PROBLEM 4.5 ----------------------- 
-% Demonstrate how lick rate changes in response to Ensure rewards
+%% Quantify how lick rate changes in response to Ensure rewards
 
 
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+## NGP MATLAB Bootcamp Repo
+This repository holds the code for the NGP MATLAB bootcamp.
+
+### Directory structure
+* `2025/` holds the course content as given in 2025 (largely written by Katie Furman)
+* `data` holds data for students to find and download
+* `presentations` holds the PowerPoint delivered at the beginning of the module
+* `problem sets` holds the problem sets
